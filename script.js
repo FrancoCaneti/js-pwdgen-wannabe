c6e873077024a074d6b54abbbca956425e1649c2
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-18 17:30:02 +0100

    jv

---
+++ script.js
@@ -0,0 +1,17 @@
+//1 chiedi nome
+var nome = prompt ('Qual è il tuo nome?');
+console.log(nome);
+
+
+//2 chiedi cognome
+var cognome = prompt ('Qual è il tuo cognome?');
+console.log(cognome);
+
+//3 chiedi colore
+var colore = prompt ('Qual è il tuo colore preferito?');
+console.log(colore);
+
+
+//4 Genera password
+
+document .getElementById('main-title').innerHTML ='La password è: ' + nome + cognome + colore
